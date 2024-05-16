cba868aa679437c0760419a074f5de9f29a5f174
Topias Ukkonen <ukkonentopias@gmail.com>
2024-05-16 09:52:57 +0300

    main.py changes

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from compressor.compressor import check
 from sys import argv
-from compressor.generators.gentxt import generate_binary_file
 
 if __name__ == "__main__":
-    generate_binary_file()
     if len(argv) != 2:
         print("Usage: python3 main.py file_path")
     else:
